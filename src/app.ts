30a61f4ed0e47c42abbeed84c970f2ef9e8ecfa7
Clecio Silva <clecio-sf@hotmail.com>
2026-06-02 20:07:54 -0300

    feat: setup fastify app and server entrypoint

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify'
+import { fastifyJwt } from '@fastify/jwt'
+import { env } from './env'
+import { ZodError } from 'zod'
+
+export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET
+})
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(404)
+      .send({ message: 'Validation Error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
