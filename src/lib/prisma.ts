02387520faa2f1e431f0a12a8e1e2e2add14f2ee
Clecio Silva <clecio-sf@hotmail.com>
2026-06-03 22:46:52 -0300

    fix: configure prisma postgres adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,10 @@
 import { env } from '@/env'
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL })
 
 export const prisma = new PrismaClient({
+  adapter,
   log: env.NODE_ENV === 'dev' ? ['query'] : [],
 })
