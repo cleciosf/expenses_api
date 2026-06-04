ad6d03c40d61e4b039e3945127694760a61f8b43
Clecio Silva <clecio-sf@hotmail.com>
2026-06-03 22:47:31 -0300

    feat: add user registration route

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import fastify from 'fastify'
 import { fastifyJwt } from '@fastify/jwt'
 import { env } from './env'
 import { ZodError } from 'zod'
+import { userRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
+
+app.register(userRoutes)
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET
