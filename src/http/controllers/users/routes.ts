8deb262f945e2fa14554517cb5c75e01b8c5d8eb
Clecio Silva <clecio-sf@hotmail.com>
2026-06-16 19:15:20 -0300

    refactor: rename soft-delete controller to follow naming conventions

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -6,7 +6,7 @@ import { refresh } from "./refresh";
 import { profile } from './profile'
 import { updateUser } from "./update";
 import { changePassword } from "./change-password";
-import { softDelete } from "./soft-delete-use-case";
+import { softDelete } from "./soft-delete";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
